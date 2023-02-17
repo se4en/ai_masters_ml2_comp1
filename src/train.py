c97eb82ec003b2edc5ef655273d5014be9200cc1
motrichdo <001122mj@gmail.com>
2023-02-17 23:58:57 +0300

    Small updates on pipeline setup

--- src/train.py
+++ src/train.py
@@ -1,15 +1,18 @@
 import os
-from time import sleep
-from omegaconf import DictConfig
 import warnings
 
 import pandas as pd
 import numpy as np
+from omegaconf import DictConfig
 import hydra
 from hydra.utils import instantiate
 import mlflow
 
-from utils import log_params_from_omegaconf_dict, create_submission
+from utils import (
+    log_params_from_omegaconf_dict,
+    create_submission,
+    save_feature_importance,
+)
 from data.data_laoder import load_data
 
 warnings.simplefilter(action="ignore", category=pd.errors.SettingWithCopyWarning)
@@ -25,17 +28,18 @@ def main(cfg: DictConfig) -> float:
 
     X_train, y_train = load_data(train_df)
     X_test, y_test = load_data(test_df)
+    test_ids = X_test["id"].copy()
 
     cv = instantiate(cfg.cv)
     feature_generator = instantiate(cfg.feature_generator)
     metric = instantiate(cfg.metric)
 
-    with mlflow.start_run():
+    with mlflow.start_run() as cur_mlflow_run:
         log_params_from_omegaconf_dict(cfg)
         train_scores = []
         val_scores = []
 
-        for train_idxs, val_idxs in cv(X_train, y_train):
+        for train_idxs, val_idxs in cv.split(X_train, X_train["product_type"]):
             _X_train, _y_train = X_train.iloc[train_idxs], y_train.iloc[train_idxs]
             _X_val, _y_val = X_train.iloc[val_idxs], y_train.iloc[val_idxs]
             _X_train, _X_val, _ = feature_generator.process_features(
@@ -52,22 +56,36 @@ def main(cfg: DictConfig) -> float:
         train_score = np.mean(train_scores)
         val_score = np.mean(val_scores)
         mlflow.log_metric("train_metric", train_score)
+        for i, cv_value in enumerate(val_scores):
+            mlflow.log_metric(f"_val_metric_{i}", cv_value)
         mlflow.log_metric("val_metric", val_score)
 
         # create submission
         model = instantiate(cfg.model)
         X_train, X_test, _ = feature_generator.process_features(X_train, X_test)
         model.fit(X_train, y_train)
+
         hydra_cfg = hydra.core.hydra_config.HydraConfig.get()
         submission_path = os.path.join(
-            hydra_cfg["runtime"]["output_dir"], "submission.csv"
+            hydra_cfg["runtime"]["output_dir"],
+            f"subm_{cur_mlflow_run.info.run_name}.csv",
         )
         create_submission(
-            X_test,
+            test_ids,
             model.predict(X_test),
             submission_path,
         )
         mlflow.log_artifact(submission_path)
+
+        importance_path = os.path.join(
+            hydra_cfg["runtime"]["output_dir"], "importance.png"
+        )
+        save_feature_importance(
+            model,
+            X_train.columns.tolist(),
+            importance_path,
+        )
+        mlflow.log_artifact(importance_path)
 
     return val_score
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,12 @@
+import typing as t
+
 from omegaconf import DictConfig, ListConfig
 import mlflow
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from lightgbm import LGBMRegressor
+from catboost import CatBoostRegressor
 
 
 def log_params_from_omegaconf_dict(params):
@@ -20,10 +26,24 @@ def _explore_recursive(parent_name, element):
             mlflow.log_param(f"{parent_name}.{i}", v)
 
 
-def create_submission(
-    X: pd.DataFrame, y_pred: pd.Series, fname: str = "submission.csv"
-):
-    subm_df = pd.DataFrame({"id": X["id"].tolist()})
+def create_submission(ids: pd.Series, y_pred: pd.Series, fname: str = "submission.csv"):
+    subm_df = pd.DataFrame({"id": ids})
     subm_df["result_price"] = y_pred
 
     subm_df.to_csv(fname, index=False)
+
+
+def save_feature_importance(
+    model: t.Union[LGBMRegressor, CatBoostRegressor], f_names: t.List[str], fname: str
+) -> None:
+    feature_imp = pd.DataFrame(
+        {"Value": model.feature_importances_, "Feature": f_names}
+    )
+    plt.figure(figsize=(20, 10))
+    sns.barplot(
+        x="Value",
+        y="Feature",
+        data=feature_imp.sort_values(by="Value", ascending=False),
+    )
+    plt.savefig(fname)
+    plt.close()
